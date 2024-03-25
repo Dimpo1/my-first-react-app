5ebbfd40314433b780fa853489653ef17f419dce
Dimpo1 <111021090+Dimpo1@users.noreply.github.com>
2024-03-25 18:10:06 +0200

    transfering web-app to react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Appfooter from "./Appfooter";
+
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Appfooter />
+    </div>
+  );
+}
